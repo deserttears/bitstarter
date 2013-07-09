91bfd9b5ae0e43e1dd053424216a54f9abe93b4e
deserttears <deserttears@hotmail.com>
2013-07-09 02:13:25 +0000

    Updated web.js to read index.html.

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs"');
+
 
 var app = express.createServer(express.logger());
 
+// read file with file system, which returns a buffer
+var content_buf = fs.readFileSync('index.html', 'utf8');
+
+// save content from file to buffer
+var content = content_buf.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  reponse.send(content);
 });
 
 var port = process.env.PORT || 5000;
